18cfea353678d510052480c389c13efdcfab96b2
Allyn <allyn0306@gmail.com>
2017-11-17 18:32:58 +0800

    数据绑定

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,3 +1,25 @@
 Page({
+  data:{
+    date:"Jan 28 2017",
+    title:"小时候的冰棍儿与雪糕",
+    postImg:"/images/post/post-4.jpg",
+    avatar:"/images/avatar/avatar-5.png",
+    content:"冰棍与雪糕绝对不是同一个东西。3到5毛钱的雪糕犹如现在的哈根达斯，而5分1毛的冰棍就像现在的老冰棒。时过境迁，。。。",
+    readingNum:92,
+    collectionNum:108,
+    commentNum:7
+  },
+  onLoad:function(){
+    this.setData({
+      title:"通过onload来设置data"
+    })
+  },
+  onShow:function(){
+
+  },
+  onReady:function(){
+
+  }
+
 
 })
